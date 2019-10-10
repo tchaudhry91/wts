bad745279bc60fed25d33897b0f30bf2cfce622e
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2019-10-10 14:04:22 +0000

    Added bolt db implementation

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	cloud.google.com/go v0.46.3 // indirect
 	cloud.google.com/go/firestore v1.0.0
+	github.com/boltdb/bolt v1.3.1
 	github.com/go-kit/kit v0.9.0
 	github.com/pkg/errors v0.8.1
 )
--- svc/store.go
+++ svc/store.go
@@ -3,7 +3,10 @@ package svc
 import (
 	"cloud.google.com/go/firestore"
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/boltdb/bolt"
+	"github.com/pkg/errors"
 	"github.com/tchaudhry91/wts-service/wts"
 )
 
@@ -14,10 +17,78 @@ type ScriptRecord struct {
 	Script *wts.Script `json:"script,omitempty"`
 }
 
+var dbError = errors.New("Failed on a database operation")
+var notFound = errors.New("Record not found")
+
 // Store is an interface that allows repository actions for Scripts
 type Store interface {
 	Put(ctx context.Context, user string, record *ScriptRecord) error
 	Get(ctx context.Context, user string, name string) (*ScriptRecord, error)
+}
+
+// BoltStore is a single file database meant for single service setups
+type BoltStore struct {
+	DB *bolt.DB
+}
+
+// Put stores the record in the given bolt db
+func (s *BoltStore) Put(ctx context.Context, user string, record *ScriptRecord) error {
+	// Start a write transaction
+	if err := s.DB.Update(func(tx *bolt.Tx) error {
+		buc, err := tx.CreateBucketIfNotExists([]byte("scripts"))
+		if err != nil {
+			return err
+		}
+
+		// Store the JSON marshalled record
+		key := "user" + "/" + record.Script.Name
+		val, err := json.Marshal(record)
+		if err != nil {
+			return err
+		}
+		if err := buc.Put([]byte(key), val); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return errors.Wrap(dbError, err.Error())
+	}
+	return nil
+}
+
+// Get retreives a record from the given bolt db
+func (s *BoltStore) Get(ctx context.Context, user string, name string) (*ScriptRecord, error) {
+	record := &ScriptRecord{}
+	// Read the value for the given key
+	key := user + "/" + name
+	if err := s.DB.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("scripts"))
+		if b == nil {
+			return notFound
+		}
+		data := b.Get([]byte(key))
+		if data == nil {
+			return notFound
+		}
+		if errIn := json.Unmarshal(data, record); errIn != nil {
+			return errors.Wrap(dbError, errIn.Error())
+		}
+		return nil
+	}); err != nil {
+		return record, err
+	}
+	return record, nil
+}
+
+// Returns a new Bolt based Store implementation for WTS
+func NewBoltStore(fname string) (*BoltStore, error) {
+	store := &BoltStore{}
+	db, err := bolt.Open(fname, 0600, nil)
+	if err != nil {
+		return store, err
+	}
+	store.DB = db
+	return store, nil
 }
 
 // WTSFirestore provides a store implementation via Google Firestore
